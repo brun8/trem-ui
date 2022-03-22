6dab42cde433a66381d0795d44d6a0f703fd87ca
bruno <brunofvteixeira@gmail.com>
2022-03-22 13:05:22 -0300

    form page

--- src/pages/l/[id].tsx
+++ src/pages/l/[id].tsx
@@ -1,29 +1,50 @@
 import logo from 'assets/logo.png';
 import Image from 'next/image';
-import { GetStaticPropsContext } from 'next';
+import { GetServerSideProps } from 'next';
+
+import { gql } from '@apollo/client';
+import client from 'lib/apollo-client';
 
 export default function FormPage() {
   return (
     <div className="flex flex-col h-screen bg-slate-400 gap-16">
-      <nav className="h-28 flex justify-between px-16 items-center">
-        <h1 className="text-4xl font-bold">
-          Nome da empresa
-        </h1>
-        <div>
-          <Image src={logo} alt='Blue Bridge' height={55} width={252} />
-        </div>
-      </nav>
-      <div className='h-auto flex flex-col items-center'>
-        <h2 className='font-semibold text-2xl uppercase'>
-          lista de documentos
-        </h2>
-      </div>
+
     </div>
   );
 }
 
-export async function GetStaticProps(context: GetStaticPropsContext) {
-  const id = context.params?.id;
+export async function getServerSideProps() {
+  const id = "cl114pznj0095zpi0alzoimq7";
+
+  const { data } = await client.query({
+    query: gql`
+      query {
+        list(where: {id:  "cl114pznj0095zpi0alzoimq7"}) {
+          id
+          title
+          client {
+            name
+          }
+          type {
+            files {
+              name
+              extensions
+            }
+          }
+          uploads {
+            file {
+              filename
+            }
+            fileType {
+              name
+            }
+          }
+        }
+      }
+    `,
+  });
+
+  console.log(data);
 
   return {
     props: {}
