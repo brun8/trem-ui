b3d8c113f413acc4950dac7ab99f546225e01b54
bruno <brunofvteixeira@gmail.com>
2022-03-23 12:49:28 -0300

    wip: form page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,10 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&family=Lexend:wght@300;400;600;700&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Karla:wght@300;400;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
--- src/pages/l/[id].tsx
+++ src/pages/l/[id].tsx
@@ -1,25 +1,51 @@
-import logo from 'assets/logo.png';
-import Image from 'next/image';
-import { GetServerSideProps } from 'next';
+import type { GetServerSidePropsContext } from 'next';
 
 import { gql } from '@apollo/client';
 import client from 'lib/apollo-client';
 
-export default function FormPage() {
-  return (
-    <div className="flex flex-col h-screen bg-slate-400 gap-16">
 
+export default function FormPage({ list }: any) {
+  console.log(list);
+  return (
+    <div className="flex flex-col gap-8 h-full p-12 items-center">
+      <div className='flex p-12 justify-start w-full'>
+        <h1 className='font-bold text-4xl'>
+          { list.title }
+        </h1>
+      </div>
+      <div className='w-4/5 max-w-xl border-2 border-black p-8 rounded-xl'>
+        <div className='grid gap-4 items-center' style={{gridTemplateColumns: "1fr auto auto auto"}}>
+          {list.type.files.map((file: any) => (
+            <>
+              <div key={file.id} className="px-4 text-lg font-semibold">
+                {file.name}
+              </div>
+              <div>
+                {file.extensions}
+              </div>
+              <div>
+                <button
+                  className='p-2 text-white bg-blue-600 rounded-lg border-2 border-black font-bold'
+                  onClick={() => console.log('aqui')}>
+                  upload
+                </button>
+              </div>
+              <div></div>
+            </>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
 
-export async function getServerSideProps() {
-  const id = "cl114pznj0095zpi0alzoimq7";
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const id = context.params?.id;
 
   const { data } = await client.query({
     query: gql`
       query {
-        list(where: {id:  "cl114pznj0095zpi0alzoimq7"}) {
+        list(where: {id: "${id}"}) {
           id
           title
           client {
@@ -27,6 +53,7 @@ export async function getServerSideProps() {
           }
           type {
             files {
+              id
               name
               extensions
             }
@@ -44,10 +71,10 @@ export async function getServerSideProps() {
     `,
   });
 
-  console.log(data);
-
   return {
-    props: {}
+    props: {
+      list: data.list,
+    }
   }
 }
 
